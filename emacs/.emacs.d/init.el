4db902b35da8d1a8044d0b34ef7b29132bd7b97f
Matúš Honěk <mhonek@redhat.com>
2016-08-18 14:32:19 +0200

    [emacs] reindent and move around, non-functional

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -116,144 +116,98 @@
 
 
 ;;; packages
-(use-package ido
-  :ensure t
-  :diminish ido-mode
-  :config (progn
-	    (ido-mode 1)
-	    (ido-everywhere 1)
-	    (global-set-key "\M-x"
-			    (lambda () (interactive)
-			      (call-interactively
-			       (intern
-				(ido-completing-read "M-x "
-						     (all-completions "" obarray 'commandp))))))))
+(use-package ido :ensure t :diminish ido-mode :config
+  (progn
+    (ido-mode 1)
+    (ido-everywhere 1)
+    (global-set-key "\M-x"
+		    (lambda () (interactive)
+		      (call-interactively
+		       (intern
+			(ido-completing-read "M-x "
+					     (all-completions "" obarray 'commandp))))))))
+
+(use-package flx-ido :ensure t :diminish flx-ido-mode :config
+  (progn
+    (setq ido-enable-flex-matching t)
+    (setq ido-use-faces nil)))
+
+(use-package undo-tree :ensure t :diminish undo-tree-mode :init
+  (progn
+    (global-undo-tree-mode)
+    (setq undo-tree-visualizer-timestamps t)
+    (setq undo-tree-visualizer-diff t)))
+
+(use-package which-key :ensure t :diminish which-key-mode :config
+  (which-key-mode))
+
+(use-package projectile :ensure t :diminish projectile-mode :config
+  (projectile-global-mode))
+
+(use-package org :ensure t :config
+  (progn
+    ;; custom <-ish templates (use like `<s' and TAB, or `<se' and TAB)
+    (add-to-list 'org-structure-template-alist
+		 '("s" "#+NAME: ?\n#+BEGIN_SRC \n\n#+END_SRC"))
+    (add-to-list 'org-structure-template-alist
+		 '("se" "#+NAME: ?\n#+BEGIN_SRC emacs-lisp\n\n#+END_SRC"))
+    ;; active Babel languages
+    (require 'ob-sh)
+    (require 'ob-python)
+    (org-babel-do-load-languages
+     'org-babel-load-languages
+     '((sh . t)
+       (emacs-lisp . t)
+       (python . t)
+       (gnuplot . t)))))
 
 
-(use-package flx-ido
-  :ensure t
-  :diminish flx-ido-mode
-  :config (progn
-	    (setq ido-enable-flex-matching t)
-	    (setq ido-use-faces nil)))
+(use-package magit  :ensure t :diminish magit-mode :config
+  (progn
+    (add-hook 'prog-mode 'magit-mode)))
 
+(use-package git-gutter-fringe :ensure t :diminish git-gutter-mode :config
+  (global-git-gutter-mode t))
 
-(use-package undo-tree
-  :ensure t
-  :diminish undo-tree-mode
-  :init (progn
-          (global-undo-tree-mode)
-          (setq undo-tree-visualizer-timestamps t)
-          (setq undo-tree-visualizer-diff t)))
+(use-package rpm-spec-mode :ensure t :config
+  (progn
+    (autoload 'rpm-spec-mode "rpm-spec-mode.el" "RPM spec mode." t)
+    (setq auto-mode-alist (append '(("\\.spec" . rpm-spec-mode))
+				  auto-mode-alist))))
 
-
-(use-package "which-key"
-  :ensure t
-  :diminish which-key-mode
-  :config (which-key-mode))
-
-
-(use-package "projectile"
-  :ensure t
-  :diminish projectile-mode
-  :config (projectile-global-mode))
-
-
-(use-package "org"
-  :ensure t
-  :config (progn
-			;; custom <-ish templates (use like `<s' and TAB, or `<se' and TAB)
-            (add-to-list 'org-structure-template-alist
-                         '("s" "#+NAME: ?\n#+BEGIN_SRC \n\n#+END_SRC"))
-            (add-to-list 'org-structure-template-alist
-                         '("se" "#+NAME: ?\n#+BEGIN_SRC emacs-lisp\n\n#+END_SRC"))
-			;; active Babel languages
-			(require 'ob-sh)
-			(require 'ob-python)
-			(org-babel-do-load-languages
-			 'org-babel-load-languages
-			 '((sh . t)
-			   (emacs-lisp . t)
-			   (python . t)
-			   (gnuplot . t)))))
-
-
-(use-package "magit"
-  :ensure t
-  :diminish magit-mode
-  :config (progn
-            (add-hook 'prog-mode 'magit-mode)))
-
-
-(use-package "git-gutter-fringe"
-  :ensure t
-  :diminish git-gutter-mode
-  :config (global-git-gutter-mode t))
-
-
-(use-package "rpm-spec-mode"
-  :ensure t
-  :config (progn
-            (autoload 'rpm-spec-mode "rpm-spec-mode.el" "RPM spec mode." t)
-            (setq auto-mode-alist (append '(("\\.spec" . rpm-spec-mode))
-                                          auto-mode-alist))))
-
-
-(use-package "company"
-  :ensure t
-  :commands global-company-mode
-  :diminish company-mode
+(use-package company :ensure t :diminish company-mode
+  :commands global-company-mode 
   :init (global-company-mode)
-  :config (progn
-            (setq company-tooltip-limit 20
-                  company-idle-delay .3
-                  company-echo-delay 0
-                  company-begin-commands '(self-insert-command))))
+  :config
+  (progn
+    (setq company-tooltip-limit 20
+	  company-idle-delay .3
+	  company-echo-delay 0
+	  company-begin-commands '(self-insert-command))))
 
+(use-package outshine :ensure t :config
+  (add-hook 'outline-minor-mode-hook 'outshine-hook-function))
 
-(use-package "outshine"
-  :ensure t
-  :config (add-hook 'outline-minor-mode-hook 'outshine-hook-function))
+(use-package paredit :ensure t :diminish paredit-mode :config
+  (add-hook 'emacs-lisp-mode-hook 'paredit-mode))
 
+(use-package eldoc :diminish eldoc-mode :config
+  (add-hook 'emacs-lisp-mode-hook 'eldoc-mode))
 
-(use-package "paredit"
-  :ensure t
-  :diminish paredit-mode
-  :config (add-hook 'emacs-lisp-mode-hook 'paredit-mode))
+(use-package cider :ensure t)
 
-(use-package "eldoc"
-  :diminish eldoc-mode
-  :config (add-hook 'emacs-lisp-mode-hook 'eldoc-mode))
+(use-package f :ensure t)
 
+(use-package highlight-thing :ensure t :diminish highlight-thing-mode :config
+  (highlight-thing-mode))
 
-(use-package "cider"
-  :ensure t)
+(use-package highlight-symbol :ensure t)
 
+(use-package highlight-parentheses :ensure t :diminish highlight-parentheses-mode :config
+  (global-highlight-parentheses-mode))
 
-(use-package "f"
-  :ensure t)
-
-
-(use-package "highlight-thing"
-  :ensure t
-  :diminish highlight-thing-mode
-  :config (highlight-thing-mode))
-
-
-(use-package "highlight-symbol"
-  :ensure t)
-
-
-(use-package "highlight-parentheses"
-  :ensure t
-  :diminish highlight-parentheses-mode
-  :config (global-highlight-parentheses-mode))
-
-
-(use-package "gnuplot"
-  :ensure t
-  :config (require 'ob-gnuplot))
-
+(use-package "gnuplot" :ensure t :config
+  (require 'ob-gnuplot))
 
 (use-package vdiff :ensure t)
 
