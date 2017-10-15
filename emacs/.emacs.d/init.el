fde9d98ef8cbb3994989305ffe23f49f38547fbc
Matúš <matushonek@gmail.com>
2017-10-15 18:20:41 +0200

    [emacs] a lot of updates

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,6 +6,8 @@
         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
+(defalias 'yes-or-no-p 'y-or-n-p)
+
 ;;; init req-package
 (unless (package-installed-p 'req-package)
   (package-refresh-contents)
@@ -17,11 +19,11 @@
   (package-refresh-contents)
   (package-install 'use-package))
 
+(setq load-prefer-newer t)
+
 ;; general use packages
-(use-package "dash"
-  :ensure t)
-(use-package "s"
-  :ensure t)
+(use-package "dash" :ensure t)
+(use-package "s" :ensure t)
 
 (if (display-graphic-p)
     (progn      
@@ -31,41 +33,52 @@
         ;(desktop-save-mode 1)
         (setq confirm-kill-emacs 'y-or-n-p))))
 
-(use-package "solarized-theme"
-  :ensure t
-  :config (progn
-            (setq solarized-scale-org-headlines nil
-                  solarized-high-contrast-mode-line t
-                  solarized-use-variable-pitch nil
-                  solarized-distinct-fringe-background t
-		  solarized-distinct-doc-face t
-		  solarized-emphasize-indicators t)
-            (load-theme 'solarized-light t)))
+(req-package solarized-theme :config
+  (progn
+    (setq solarized-scale-org-headlines nil
+	  solarized-high-contrast-mode-line t
+	  solarized-use-variable-pitch nil
+	  solarized-distinct-fringe-background t
+	  solarized-distinct-doc-face t
+	  solarized-emphasize-indicators t)
+    (load-theme 'solarized-light t)
+    (setq cursor-type 'box
+	  use-dialog-box nil
+	  mouse-yank-at-point t)
+    (cl-flet ((first-eligible-font-of
+	       (lambda (fonts)
+		 (--first (member it (font-family-list)) fonts))))
+      (let ((ff-mono (first-eligible-font-of '("Liberation Mono" "Terminus" "Terminal")))
+	    (ff-sans (first-eligible-font-of '("Liberation Sans"))))
+	(message ff-mono)
+	(set-face-attribute 'default nil :height 100 :family ff-mono :foreground "#000000" :background "#f8f8ee")
+	(set-face-attribute 'cursor nil :background "orange")
+	(set-face-attribute 'mode-line nil :height 0.9 :family ff-sans)
+	(set-face-attribute 'mode-line-inactive nil :inherit 'mode-line)
+	(set-face-attribute 'speedbar-button-face nil :height 0.8 :family ff-sans)
+	(set-face-attribute 'speedbar-file-face nil :height 0.8 :family ff-sans)
+	(set-face-attribute 'speedbar-directory-face nil :height 0.8 :family ff-sans)
+	(set-face-attribute 'speedbar-highlight-face nil :height 0.8 :family ff-sans)
+	(set-face-attribute 'speedbar-selected-face nil :height 0.8 :family ff-sans)
+	(set-face-attribute 'speedbar-separator-face nil :height 0.8 :family ff-sans)
+	(set-face-attribute 'speedbar-tag-face nil :height 0.8 :family ff-sans)))))
 
-(setq ff-mono "Liberation Mono"
-      ff-sans "Liberation Sans")
 
 (ignore-errors
   (menu-bar-mode -1)
   (tool-bar-mode -1)
-  (scroll-bar-mode -1)
-  (setq cursor-type '(hbar . 3)
-	use-dialog-box nil)
-  (set-face-attribute 'default nil :height 100 :family ff-mono :foreground "#000000")
-  (set-face-attribute 'mode-line nil :height 0.8 :family ff-sans)
-  (set-face-attribute 'mode-line-inactive nil :inherit 'mode-line)
-  (set-face-attribute 'speedbar-button-face nil :height 0.8 :family ff-sans)
-  (set-face-attribute 'speedbar-file-face nil :height 0.8 :family ff-sans)
-  (set-face-attribute 'speedbar-directory-face nil :height 0.8 :family ff-sans)
-  (set-face-attribute 'speedbar-highlight-face nil :height 0.8 :family ff-sans)
-  (set-face-attribute 'speedbar-selected-face nil :height 0.8 :family ff-sans)
-  (set-face-attribute 'speedbar-separator-face nil :height 0.8 :family ff-sans)
-  (set-face-attribute 'speedbar-tag-face nil :height 0.8 :family ff-sans))
+  (scroll-bar-mode -1))
+
+;(speedbar-add-supported-extension ".ldif")
+
+(hl-line-mode 1)
 
 ;;; setup basic
 (setf inhibit-startup-screen t)
 (setf show-trailing-whitespace t)
 (setq confirm-kill-emacs 'y-or-n-p)
+
+(setq browse-url-browser-function 'browse-url-xdg-open)
 
 (setq backup-by-copying t  ; don't clobber symlinks
       backup-directory-alist
@@ -90,6 +103,8 @@
 (global-set-key (kbd "C-M-+") 'text-scale-increase)
 (global-set-key (kbd "C-M-_") 'text-scale-decrease)
 
+(global-auto-revert-mode 1)
+(add-hook 'dired-mode-hook 'auto-revert-mode)
 (add-hook 'dired-mode-hook 'hl-line-mode)
 
 (global-set-key [next] (lambda () (interactive)
@@ -118,6 +133,13 @@
 (setq tab-always-indent 'complete)
 (add-to-list 'completion-styles 'initials t)
 
+(use-package highlight-quoted :ensure t :config
+  (add-hook 'emacs-lisp-mode-hook 'highlight-quoted-mode))
+
+(use-package highlight-stages :ensure t :config
+  (progn
+    ))
+
 (defun k/sm-greek-lambda ()
   (font-lock-add-keywords nil `(("\\<lambda\\>"
                                  (0 (progn (compose-region (match-beginning 0) (match-end 0)
@@ -137,11 +159,19 @@
 (require 'semantic/ia)  ; adds more completion options
 (require 'semantic/bovine/gcc)  ; use gcc for from system headers completion
 
-(add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-decoration-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-highlight-func-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-breadcrumbs-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-idle-completions-mode)
 (add-to-list 'semantic-default-submodes 'global-semantic-idle-local-symbol-highlight-mode)
 (add-to-list 'semantic-default-submodes 'global-semantic-idle-scheduler-mode)
 (add-to-list 'semantic-default-submodes 'global-semantic-idle-summary-mode)
-(add-to-list 'semantic-default-submodes 'global-semantic-idle-breadcrumbs-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-mru-bookmark-mode)
+(add-to-list 'semantic-default-submodes 'global-semantic-stickyfunc-mode)
+(add-to-list 'semantic-default-submodes 'global-semanticdb-minor-mode)
+(semantic-mode 1)
+(use-package stickyfunc-enhance :ensure t :config
+  (require 'stickyfunc-enhance))
 
 
 
@@ -152,9 +182,13 @@
 
 (req-package el-get ;; prepare el-get (optional)
   :force t ;; load package immediately, no dependency resolution
-  :config (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/el-get/recipes")
-  (el-get 'sync))
+  :config
+  (progn (add-to-list 'el-get-recipe-path "~/.emacs.d/el-get/el-get/recipes")
+	 (el-get 'sync)
+	 (require 'el-get-bundle)))
 
+
+(use-package strace-mode :ensure t)
 
 (use-package smart-tabs-mode :ensure t :config
   (progn
@@ -178,6 +212,7 @@
           helm-M-x-fuzzy-match t)
 
     (global-set-key (kbd "C-c h") 'helm-command-prefix)
+    (which-key-add-key-based-replacements "C-c h" "helm")
     (global-unset-key (kbd "C-x c"))
 
     (define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
@@ -196,13 +231,37 @@
     (use-package helm-swoop :ensure t :config
       (progn
 	(global-set-key (kbd "M-i") 'helm-swoop)
+	(global-set-key (kbd "M-I") 'helm-multi-swoop-projectile)
 	(setq helm-swoop-split-with-multiple-windows t
 	      helm-swoop-split-direction 'split-window-vertically
 	      helm-swoop-use-line-number-face t)))
 
     (helm-mode 1)))
 
-(use-package which-key :ensure t :diminish which-key-mode :config (which-key-mode))
+(use-package which-key :ensure t :diminish which-key-mode :config
+  (progn
+    (set-face-attribute 'which-key-key-face nil :height 1.0)
+    (set-face-attribute 'which-key-separator-face nil :height 1.0)
+    (set-face-attribute 'which-key-note-face nil :height 1.0)
+    (set-face-attribute 'which-key-special-key-face nil :height 1.0)
+    (set-face-attribute 'which-key-group-description-face nil :height 1.0)
+    (set-face-attribute 'which-key-command-description-face nil :height 1.0)
+    (set-face-attribute 'which-key-local-map-description-face nil :height 1.0)
+    (setq-default which-key-replacement-alist '(((nil . "^c-") . (nil . "c."))
+						((nil . "ggtags-") . (nil . "gg."))
+						((nil . "paredit-") . (nil . "()."))
+						((nil . "projectile-") . (nil . "p."))
+						((nil . "helm-") . (nil . "h."))))
+    (which-key-add-key-based-replacements "C-x a" "abbrev")
+    (which-key-add-key-based-replacements "C-x n" "narrow")
+    (which-key-add-key-based-replacements "C-x r" "register/rectangle")
+    (which-key-add-key-based-replacements "C-x w" "highlight")
+    (which-key-add-key-based-replacements "C-x X" "edebug")
+    (which-key-add-key-based-replacements "C-x @" "event-apply")
+    (which-key-add-key-based-replacements "M-g" "goto")
+
+
+    (which-key-mode)))
 
 
 (use-package auto-complete :ensure t :config (ac-config-default))
@@ -251,34 +310,54 @@
 	  projectile-mode-line '(:propertize
 				 (:eval (if (file-remote-p default-directory)
 					    " Pr"
-					  (format " %s (%s)"
+					  (format " %s@%s@%s"
 						  (projectile-project-name)
+						  (or (magit-get-current-branch)
+						      (magit-get-current-tag))
 						  (let ((fn tags-file-name))
 						    (if (stringp fn)
 							(file-relative-name fn)
-						      "?")))))
+						      "(void)")))))
 				 face modeline-buffer-id))
+    (which-key-add-key-based-replacements "C-c p" "projectile")
     (use-package ibuffer-projectile :ensure t)
-    ;; (use-package projectile-speedbar :ensure t :config
-    ;;   (progn
-    ;;     (global-set-key (kbd "M-<f2>") 'projectile-speedbar-toggle)
-    ;;     (setq projectile-speedbar-projectile-speedbar-enable nil)))
+    (use-package projectile-speedbar :ensure t
+      :config (global-set-key (kbd "M-<f2>") 'projectile-speedbar-toggle)
+      :init (setq projectile-speedbar-enable nil))
+    (defadvice projectile-run-shell (around k/projectile-run-shell-outside-project activate)
+      "adds a possibility to open the shell even outside of a project"
+      (interactive)
+      (let ((projectile-require-project-root nil))
+	ad-do-it))
     (projectile-cleanup-known-projects)
     (projectile-global-mode)))
 
 (use-package magit :ensure t :diminish magit-mode :config
   (progn
-    (global-set-key (kbd "C-c g") 'magit-status)))
+    (global-set-key (kbd "C-c g g") 'magit-status)
+    (global-set-key (kbd "C-c g b") 'magit-blame)
+    (which-key-add-key-based-replacements "C-c g" "magit")))
 
 (use-package ggtags :ensure t :config
   (progn
     (add-hook 'c-mode-common-hook
 	      (lambda () (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
-		      (ggtags-mode 1))))))
+		      (ggtags-mode 1))))
+    (define-key ggtags-mode-map (kbd "C-c t s") 'ggtags-find-other-symbol)
+    (define-key ggtags-mode-map (kbd "C-c t h") 'ggtags-view-tag-history)
+    (define-key ggtags-mode-map (kbd "C-c t r") 'ggtags-find-reference)
+    (define-key ggtags-mode-map (kbd "C-c t f") 'ggtags-find-file)
+    (define-key ggtags-mode-map (kbd "C-c t c") 'ggtags-create-tags)
+    (define-key ggtags-mode-map (kbd "C-c t u") 'ggtags-update-tags)
+
+    (define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)))
 
 (use-package git-gutter-fringe :ensure t :diminish git-gutter-mode :config (global-git-gutter-mode t))
 
-(use-package paredit :ensure t :diminish paredit-mode :config (add-hook 'emacs-lisp-mode-hook 'paredit-mode))
+(use-package paredit :ensure t :diminish paredit-mode :config
+  (progn
+    (add-hook 'emacs-lisp-mode-hook 'paredit-mode)
+    (add-hook 'scheme-mode-hook 'paredit-mode)))
 
 (use-package eldoc :diminish eldoc-mode :config (add-hook 'emacs-lisp-mode-hook 'eldoc-mode))
 
@@ -306,15 +385,23 @@
     (require 'cider)
     (setq org-babel-clojure-backend 'cider)
     (org-babel-do-load-languages 'org-babel-load-languages
-				 '((sh . t)
+				 '((shell . t)
 				   (emacs-lisp . t)
 				   (python . t)
 				   (gnuplot . t)
-				   (clojure . t)))
+				   (clojure . t)
+				   (dot . t)))
+    (add-to-list 'org-structure-template-alist '("n" "#+NAME: ?"))
     ;; do not bother when C-c C-c
     (defun kenoh/org-confirm-babel-evaluate (lang body)
-      (not (member lang (list "emacs-lisp" "clojure"))))
-    (setq org-confirm-babel-evaluate 'kenoh/org-confirm-babel-evaluate)))
+      (not (member lang (list "emacs-lisp" "clojure" "python"))))
+    (setq org-confirm-babel-evaluate 'kenoh/org-confirm-babel-evaluate)
+    (progn
+      (setq org-default-notes-file "~/kb.org")
+      (setq org-capture-templates
+	    `(("k" "Knowledge" entry (file ,(expand-file-name "~/kb.org"))
+	       "* %?%^g\n#+DATE: %t\n%i")))
+      (define-key global-map "\C-cc" 'org-capture))))
 
 (use-package yaml-mode :ensure t :config
   (progn
@@ -327,8 +414,10 @@
 	(use-package ansible-doc :ensure t :config
 	  (add-hook 'ansible-hook 'ansible-doc-mode))))))
 
-(el-get-bundle zweifisch/ob-ansible
-  (require 'ob-ansible))
+;; (el-get-bundle zweifisch/ob-ansible
+;;   (require 'ob-ansible))
+
+(req-package helm-dash)
 
 (use-package rpm-spec-mode :ensure t :config
   (progn
@@ -338,6 +427,10 @@
 
 (use-package lua-mode :ensure t)
 
+(use-package multi-term :ensure t :config
+  (progn
+    (setq multi-term-program "/bin/bash")))
+
 (use-package slime :ensure t :config
   (progn
     (setq inferior-lisp-program "sbcl"
@@ -346,14 +439,26 @@
 (use-package docker :ensure t)
 (use-package dockerfile-mode :ensure t)
 
-(use-package graphviz-dot-mode :ensure t)
+(use-package graphviz-dot-mode :ensure t :config
+  (progn
+    (setq graphviz-dot-view-command "xdot %s")))
+
+(use-package org-mind-map :ensure t)
+
+(use-package persistent-scratch :ensure t :config
+  (progn
+    (persistent-scratch-setup-default)
+    (setq persistent-scratch-autosave-interval 60)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; load private settings
 (load "~/.emacs.d/private.el" t)
 
 ;; ditch the customize feature
 (setq custom-file "~/.emacs.d/custom.el")
-;(load custom-file 'noerror)
+;;(load custom-file 'noerror)
 
-(require 'ob-ansible)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; (require 'ob-ansible)
 (req-package-finish)
