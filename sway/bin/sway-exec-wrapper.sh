6fdad158febaf62a59fe97eb99639c50d1c63e0a
Matus Honek <matushonek@gmail.com>
2020-01-18 17:28:53 +0100

    [sway] Minor changes

--- sway/bin/sway-exec-wrapper.sh
+++ sway/bin/sway-exec-wrapper.sh
@@ -6,4 +6,5 @@ log="$HOME/.k-sway_exec-$name.log"
 
 echo "STARTING at $(date)" >> $log
 trap "echo \"STOPPING at \$(date)\" >> $log" EXIT
-"${@}" >> $log 2>&1 ; echo "PROCESS EXITED with return code $?." >> $log
+set -x
+"${@}" 2>&1 | while read X; do D="$(date -Iseconds)"; echo "$X" | sed -u "s/^/[$D]  /" >> $log; done; echo "PROCESS EXITED with return code $?." >> $log
