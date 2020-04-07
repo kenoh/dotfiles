bc9c1ba1697a5f52c26e6eefd68025ebae8194eb
Matus Honek <matushonek@gmail.com>
2020-04-07 13:04:59 +0200

    [spacemacs] Init

---
+++ spacemacs/.spacemacs.d/layers/k-code/layers.el
@@ -0,0 +1,19 @@
+(configuration-layer/declare-layers
+ '(systemd
+   python
+   helm
+   auto-completion
+   better-defaults
+   emacs-lisp
+   git
+   ;; markdown
+   multiple-cursors
+   ;; org
+   ;; (shell :variables
+   ;;        shell-default-height 30
+   ;;        shell-default-position 'bottom)
+   ;; spell-checking
+   syntax-checking
+   version-control
+   treemacs
+   ))
---
+++ spacemacs/.spacemacs.d/layers/k-desk/layers.el
@@ -0,0 +1,1 @@
+(configuration-layer/declare-layer 'k-code)
